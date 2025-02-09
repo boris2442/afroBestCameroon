f70ed0385068215fd6a9183d26ec12db758c40ee
boris2442 <aubinborissimotsebo@gmail.com>
2025-02-09 17:34:56 +0100

    text

--- javascript/script.js
+++ javascript/script.js
@@ -2,13 +2,13 @@ const menu=document.querySelector("header .fa-bars");
 console.log(menu)
 const navigation=document.querySelector("header ul");
 const removeIcone=document.querySelector("header .fa-x")
-menu.addEventListener("click", ()=>{
-    navigation.classList.add("navbar");
-    console.log("text")
+// menu.addEventListener("click", ()=>{
+//     navigation.classList.add("navbar");
+//     console.log("text")
     // menu.style.display="none";
     // removeIcone.style.opacity="0";
 
-});
+// });
 // removeIcone.addEventListener("click", ()=>{
 //     navigation.classList.remove("navigation");
 //     // menu.style.display="none";
